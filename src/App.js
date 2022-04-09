70377edd96440c798c3320aec53942e67e2eb3d5
spjumde <spjumde@mitaoe.ac.in>
2022-04-09 12:07:55 +0530

    added favicon

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [color, setColor] = useState("#f15025")
   const [isColor, setIsColor] = useState(true)
   const [colorList, setColorList] = useState([])
-  const accuracy= 10
+  const accuracy = 10
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
@@ -40,7 +40,11 @@ function App() {
             <form onSubmit={handleSubmit}>
               <div className='row'>
                 <div className='col-md-8'>
-                  <input type="text" placeholder='#f15025' value={color} className={`${!isColor ? "form-control border border-danger" : "form-control"}`} onChange={((e) => setColor(e.target.value))} />
+                  <input type="text" placeholder={color} value={color} className={`${!isColor ? "form-control border border-danger" : "form-control"}`} onChange={((e) => setColor(e.target.value))} />
+                  {!isColor && <p className='text-danger mb-0'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+                    <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
+                    <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
+                  </svg> Invalid color</p>}
                 </div>
                 <div className='col-md-4 '>
                   <button type='submit' className='btn btn-info'>Submit</button>
@@ -62,7 +66,7 @@ function App() {
             return <SingleColor key={indx} {...color} hexVal={h} index={indx} length={colorList.length} />
           })}
         </div>
-      
+
       </section>
     </>
   );
